68ce397740b1adbccb72a2253acc0ac17d06d672
sea2016new <83366311@qq.com>
2016-09-09 11:20:19 +0800

    Add files via upload

---
+++ SafeUtil.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Web;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace YiHaoDian.FeifanPay
+{
+    public class SafeUtil
+    {
+        /// <summary>
+        /// 验证签名
+        /// </summary>
+        /// <param name="OriginalString">原文：UTF8编码</param>
+        /// <param name="SignatureString">签名：base64编码的字节</param>
+        /// <param name="publicKeyPath">公钥路径</param>
+        /// <returns> 验签结果</returns>
+        public bool Verify(String OriginalString, String SignatureString, String publicKeyPath)
+        {
+            //将base64签名数据转码为字节 
+            byte[] signedBase64 = Convert.FromBase64String(SignatureString);
+            byte[] orgin = Encoding.UTF8.GetBytes(OriginalString);
+
+            X509Certificate2 x509_Cer1 = new X509Certificate2(publicKeyPath,string.Empty, X509KeyStorageFlags.MachineKeySet);
+            
+
+            RSACryptoServiceProvider oRSA = new RSACryptoServiceProvider();
+            oRSA.FromXmlString(x509_Cer1.PublicKey.Key.ToXmlString(false));
+
+            bool bVerify = oRSA.VerifyData(orgin, "SHA1", signedBase64);
+            return bVerify;
+
+        }
+
+        //region 解析.net 生成的Pem
+        private static RSAParameters ConvertFromPublicKey(string pemFileConent)
+        {
+
+            byte[] keyData = Convert.FromBase64String(pemFileConent);
+            if (keyData.Length < 162)
+            {
+                throw new ArgumentException("pem file content is incorrect.");
+            }
+            byte[] pemModulus = new byte[128];
+            byte[] pemPublicExponent = new byte[3];
+            Array.Copy(keyData, 29, pemModulus, 0, 128);
+            Array.Copy(keyData, 159, pemPublicExponent, 0, 3);
+            RSAParameters para = new RSAParameters();
+            para.Modulus = pemModulus;
+            para.Exponent = pemPublicExponent;
+            return para;
+        }
+
+        /// <summary>
+        /// 验证签名
+        /// </summary>
+        /// <param name="content">需要验证的内容</param>
+        /// <param name="signedString">签名结果</param>
+        /// <param name="publicKey">公钥</param>
+        /// <param name="input_charset">编码格式</param>
+        /// <returns></returns>
+        public static bool verifyNew(string content, string signedString, string publicKey)
+        {
+            bool result = false;
+
+            Encoding code = Encoding.UTF8;
+            byte[] Data = code.GetBytes(content);
+            byte[] data = Convert.FromBase64String(signedString);
+            RSAParameters paraPub = ConvertFromPublicKey(publicKey);
+            RSACryptoServiceProvider rsaPub = new RSACryptoServiceProvider();
+            rsaPub.ImportParameters(paraPub);
+
+            SHA1 sh = new SHA1CryptoServiceProvider();
+            result = rsaPub.VerifyData(Data, sh, data);
+            return result;
+        }
+
+        private static RSACryptoServiceProvider DecodePrivateKeyInfo(byte[] pkcs8)
+        {
+
+            byte[] SeqOID = { 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00 };
+            byte[] seq = new byte[15];
+
+            MemoryStream mem = new MemoryStream(pkcs8);
+            int lenstream = (int)mem.Length;
+            BinaryReader binr = new BinaryReader(mem);    //wrap Memory Stream with BinaryReader for easy reading
+            byte bt = 0;
+            ushort twobytes = 0;
+
+            try
+            {
+
+                twobytes = binr.ReadUInt16();
+                if (twobytes == 0x8130)	//data read as little endian order (actual data order for Sequence is 30 81)
+                    binr.ReadByte();	//advance 1 byte
+                else if (twobytes == 0x8230)
+                    binr.ReadInt16();	//advance 2 bytes
+                else
+                    return null;
+
+
+                bt = binr.ReadByte();
+                if (bt != 0x02)
+                    return null;
+
+                twobytes = binr.ReadUInt16();
+
+                if (twobytes != 0x0001)
+                    return null;
+
+                seq = binr.ReadBytes(15);		//read the Sequence OID
+                if (!CompareBytearrays(seq, SeqOID))	//make sure Sequence for OID is correct
+                    return null;
+
+                bt = binr.ReadByte();
+                if (bt != 0x04)	//expect an Octet string 
+                    return null;
+
+                bt = binr.ReadByte();		//read next byte, or next 2 bytes is  0x81 or 0x82; otherwise bt is the byte count
+                if (bt == 0x81)
+                    binr.ReadByte();
+                else
+                    if (bt == 0x82)
+                    binr.ReadUInt16();
+                //------ at this stage, the remaining sequence should be the RSA private key
+
+                byte[] rsaprivkey = binr.ReadBytes((int)(lenstream - mem.Position));
+                RSACryptoServiceProvider rsacsp = DecodeRSAPrivateKey(rsaprivkey);
+                return rsacsp;
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+
+            finally { binr.Close(); }
+
+        }
+
+        private static RSACryptoServiceProvider DecodeRSAPrivateKey(byte[] privkey)
+        {
+            byte[] MODULUS, E, D, P, Q, DP, DQ, IQ;
+
+            // ---------  Set up stream to decode the asn.1 encoded RSA private key  ------
+            MemoryStream mem = new MemoryStream(privkey);
+            BinaryReader binr = new BinaryReader(mem);    //wrap Memory Stream with BinaryReader for easy reading
+            byte bt = 0;
+            ushort twobytes = 0;
+            int elems = 0;
+            try
+            {
+                twobytes = binr.ReadUInt16();
+                if (twobytes == 0x8130)	//data read as little endian order (actual data order for Sequence is 30 81)
+                    binr.ReadByte();	//advance 1 byte
+                else if (twobytes == 0x8230)
+                    binr.ReadInt16();	//advance 2 bytes
+                else
+                    return null;
+
+                twobytes = binr.ReadUInt16();
+                if (twobytes != 0x0102)	//version number
+                    return null;
+                bt = binr.ReadByte();
+                if (bt != 0x00)
+                    return null;
+
+
+                //------  all private key components are Integer sequences ----
+                elems = GetIntegerSize(binr);
+                MODULUS = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                E = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                D = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                P = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                Q = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                DP = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                DQ = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                IQ = binr.ReadBytes(elems);
+
+                // ------- create RSACryptoServiceProvider instance and initialize with public key -----
+                RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+                RSAParameters RSAparams = new RSAParameters();
+                RSAparams.Modulus = MODULUS;
+                RSAparams.Exponent = E;
+                RSAparams.D = D;
+                RSAparams.P = P;
+                RSAparams.Q = Q;
+                RSAparams.DP = DP;
+                RSAparams.DQ = DQ;
+                RSAparams.InverseQ = IQ;
+                RSA.ImportParameters(RSAparams);
+                return RSA;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally { binr.Close(); }
+        }
+
+        private static int GetIntegerSize(BinaryReader binr)
+        {
+            byte bt = 0;
+            byte lowbyte = 0x00;
+            byte highbyte = 0x00;
+            int count = 0;
+            bt = binr.ReadByte();
+            if (bt != 0x02)		//expect integer
+                return 0;
+            bt = binr.ReadByte();
+
+            if (bt == 0x81)
+                count = binr.ReadByte();	// data size in next byte
+            else
+                if (bt == 0x82)
+            {
+                highbyte = binr.ReadByte(); // data size in next 2 bytes
+                lowbyte = binr.ReadByte();
+                byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
+                count = BitConverter.ToInt32(modint, 0);
+            }
+            else
+            {
+                count = bt;     // we already have the data size
+            }
+
+
+
+            while (binr.ReadByte() == 0x00)
+            {	//remove high order zeros in data
+                count -= 1;
+            }
+            binr.BaseStream.Seek(-1, SeekOrigin.Current);		//last ReadByte wasn't a removed zero, so back up a byte
+            return count;
+        }
+
+        private static bool CompareBytearrays(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+                return false;
+            int i = 0;
+            foreach (byte c in a)
+            {
+                if (c != b[i])
+                    return false;
+                i++;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 解析java生成的pem文件私钥
+        /// </summary>
+        /// <param name="pemstr"></param>
+        /// <returns></returns>
+        private static RSACryptoServiceProvider DecodePemPrivateKey(String pemstr)
+        {
+            byte[] pkcs8privatekey;
+            pkcs8privatekey = Convert.FromBase64String(pemstr);
+            if (pkcs8privatekey != null)
+            {
+
+                RSACryptoServiceProvider rsa = DecodePrivateKeyInfo(pkcs8privatekey);
+                return rsa;
+            }
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// 签名
+        /// </summary>
+        /// <param name="content">需要签名的内容</param>
+        /// <param name="privateKey">私钥</param>
+        /// <param name="input_charset">编码格式</param>
+        /// <returns></returns>
+        public static string signNew(string content, string privateKey)
+        {
+            Encoding code = Encoding.UTF8;
+            byte[] Data = code.GetBytes(content);
+            RSACryptoServiceProvider rsa = DecodePemPrivateKey(privateKey);
+            SHA1 sh = new SHA1CryptoServiceProvider();
+            byte[] signData = rsa.SignData(Data, sh);
+            return Convert.ToBase64String(signData);
+        }
+
+        /// <summary>
+        /// 验证签名
+        /// </summary>
+        /// <param name="data">原文：UTF8编码</param>
+        /// <param name="privateKeyPath">证书路径：D:/certs/mycert.key</param>
+        /// <returns> 验签</returns>
+        public string Sign(string data, string privateKeyPath)
+        {
+            RSACryptoServiceProvider rsaCsp = LoadCertificateFile(privateKeyPath);
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            byte[] signatureBytes = rsaCsp.SignData(dataBytes, "SHA1");
+            return Convert.ToBase64String(signatureBytes);
+        }
+
+        private byte[] GetPem(string type, byte[] data)
+        {
+            string pem = Encoding.UTF8.GetString(data);
+            string header = String.Format("-----BEGIN {0}-----", type);
+            string footer = String.Format("-----END {0}-----", type);
+            int start = pem.IndexOf(header) + header.Length;
+            int end = pem.IndexOf(footer, start);
+
+            string base64 = pem.Substring(start, (end - start));
+
+            return Convert.FromBase64String(base64);
+        }
+
+        private RSACryptoServiceProvider LoadCertificateFile(string filename)
+        {
+            using (System.IO.FileStream fs = System.IO.File.OpenRead(filename))
+            {
+                byte[] data = new byte[fs.Length];
+                byte[] res = null;
+                fs.Read(data, 0, data.Length);
+                if (data[0] != 0x30)
+                {
+                    res = GetPem("RSA PRIVATE KEY", data);
+                }
+                try
+                {
+                    RSACryptoServiceProvider rsa = DecodeRSAPrivateKey(res);
+                    return rsa;
+                }
+                catch (Exception ex)
+                {
+                }
+                return null;
+            }
+        }
+    }
+}
